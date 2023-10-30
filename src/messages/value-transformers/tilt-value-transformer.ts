f03ef85deb50b67509942f739100dfa3e4f1e27b
Nikolai Sukhanov <25166404+nvsukhanov@users.noreply.github.com>
2023-10-30 20:26:38 +0400

    fix: tilt value parsing (#84)

--- src/messages/value-transformers/tilt-value-transformer.ts
+++ src/messages/value-transformers/tilt-value-transformer.ts
@@ -1,7 +1,7 @@
 import { injectable } from 'tsyringe';
 
 import { TiltData } from '../../hub';
-import { convertUint8ToSignedInt, readNumberFromUint8LEArray } from '../../helpers';
+import { convertUint16ToSignedInt, readNumberFromUint8LEArray } from '../../helpers';
 import { ITiltValueTransformer } from '../../features';
 
 @injectable()
@@ -9,13 +9,13 @@ export class TiltValueTransformer implements ITiltValueTransformer {
     public fromRawValue(
         rawValue: number[]
     ): TiltData {
-        const yaw = readNumberFromUint8LEArray(rawValue.slice(0, 1));
-        const pitch = readNumberFromUint8LEArray(rawValue.slice(2, 3));
-        const roll = readNumberFromUint8LEArray(rawValue.slice(4, 5));
+        const yaw = readNumberFromUint8LEArray(rawValue.slice(0, 2));
+        const pitch = readNumberFromUint8LEArray(rawValue.slice(2, 4));
+        const roll = readNumberFromUint8LEArray(rawValue.slice(4, 6));
         return {
-            roll: convertUint8ToSignedInt(roll),
-            pitch: convertUint8ToSignedInt(pitch),
-            yaw: convertUint8ToSignedInt(yaw)
+            roll: -convertUint16ToSignedInt(roll) % 180,
+            pitch: convertUint16ToSignedInt(pitch) % 180,
+            yaw: -convertUint16ToSignedInt(yaw) % 180
         };
     }
 }
