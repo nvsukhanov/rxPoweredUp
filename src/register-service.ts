fae319653c11443530a1d4fe8911202b5a8e5e97
Nikolai Sukhanov <25166404+nvsukhanov@users.noreply.github.com>
2023-11-19 23:49:09 +0400

    fix: make initialization of DI container explicit (#105)

---
+++ src/register-service.ts
@@ -0,0 +1,27 @@
+import { container } from 'tsyringe';
+
+import { HUB_SCANNER_ERROR_FACTORY } from './hub-scanner';
+import { HUB_CONNECTION_ERRORS_FACTORY, INBOUND_MESSAGE_LISTENER_FACTORY, InboundMessageListenerFactory, PREFIXED_CONSOLE_LOGGER_FACTORY } from './hub';
+import { HUB_PROPERTIES_FEATURE_ERRORS_FACTORY, registerFeaturesServices } from './features';
+import { ConnectionErrorFactory } from './errors';
+import { PrefixedConsoleLoggerFactory } from './logger';
+import { registerMessagesServices } from './messages';
+import { registerPortValueTransformers } from './port-value-transformers';
+
+let registered = false;
+
+export function registerServices(): void {
+    if (registered) {
+        return;
+    }
+    container.register(INBOUND_MESSAGE_LISTENER_FACTORY, InboundMessageListenerFactory);
+    container.register(HUB_PROPERTIES_FEATURE_ERRORS_FACTORY, ConnectionErrorFactory);
+    container.register(HUB_CONNECTION_ERRORS_FACTORY, ConnectionErrorFactory);
+    container.register(HUB_SCANNER_ERROR_FACTORY, ConnectionErrorFactory);
+    container.register(PREFIXED_CONSOLE_LOGGER_FACTORY, PrefixedConsoleLoggerFactory);
+
+    registerPortValueTransformers();
+    registerFeaturesServices(container);
+    registerMessagesServices(container);
+    registered = true;
+}
