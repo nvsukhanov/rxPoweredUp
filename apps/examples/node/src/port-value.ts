8f9cb91cfb16a19332e3e368cebcfcc04afb3233
Nikolai Sukhanov <nvsukhanov@outlook.com>
2024-01-15 21:04:05 +0400

    feat: add NodeJs examples, update README.md

---
+++ apps/examples/node/src/port-value.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import { audit, lastValueFrom, switchMap, takeUntil, tap } from 'rxjs';
+import { bluetooth } from 'webbluetooth';
+
+import { LogLevel, PortModeName, ValueTransformers, WELL_KNOWN_PORT_MODE_IDS, connectHub } from 'rxpoweredup';
+import { waitForPromise } from './wait-for-promise';
+
+console.log('Waiting for Hub...');
+
+const sequence = lastValueFrom(
+    connectHub(bluetooth, {
+        logLevel: LogLevel.Debug
+    }).pipe(
+        audit((hub) => hub.ports.onIoAttach({ ports: [ 0 ] })),
+        switchMap((hub) => hub.ports.portValueChanges(
+            0,
+            WELL_KNOWN_PORT_MODE_IDS.motor[PortModeName.position],
+            1,
+            ValueTransformers.position
+        ).pipe(
+            takeUntil(hub.disconnected),
+        )),
+        tap((position) => console.log(`Position: ${position}`))
+    )
+);
+
+waitForPromise(sequence);
---
+++ apps/examples/node/src/wait-for-promise.ts
@@ -0,0 +1,4 @@
+export function waitForPromise(p: Promise<unknown>): void {
+    const timeout = setTimeout(() => void 0, 0x7fffffff);
+    p.then(() => clearTimeout(timeout));
+}
